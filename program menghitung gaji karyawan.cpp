620564add72ab9d8abb55d00acc93baa14ca11f4
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:42:38 +0700

    Add files via upload

---
+++ program menghitung gaji karyawan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int jam,upah ;
+    cout<<"menghitung upah karyawan"<<endl;
+    cout<<"input jumlah jam kerja karyawan"<<endl;
+    cin>>jam;
+    upah=jam*5000;
+    cout<<"Upah yang di terima karyawan = "<<upah<<endl;
+    return 0;
+}
